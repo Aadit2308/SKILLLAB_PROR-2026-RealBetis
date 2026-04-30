297a35690a81107d7aa0ae5b03592ae6454875e1
blacksabbath2 <149758638+blacksabbath2@users.noreply.github.com>
2026-04-30 12:33:42 +0530

    Add files via upload

---
+++ code_files/design_1_wrapper.v
@@ -0,0 +1,61 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2025.2 (win64) Build 6299465 Fri Nov 14 19:35:11 GMT 2025
+//Date        : Thu Apr 30 12:25:17 2026
+//Host        : LAPTOP-SOIH342P running 64-bit major release  (build 9200)
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (iic_rtl_scl_io,
+    iic_rtl_sda_io,
+    myclk,
+    myreset,
+    uart0_rxd,
+    uart0_txd);
+  inout iic_rtl_scl_io;
+  inout iic_rtl_sda_io;
+  input myclk;
+  input myreset;
+  input uart0_rxd;
+  output uart0_txd;
+
+  wire iic_rtl_scl_i;
+  wire iic_rtl_scl_io;
+  wire iic_rtl_scl_o;
+  wire iic_rtl_scl_t;
+  wire iic_rtl_sda_i;
+  wire iic_rtl_sda_io;
+  wire iic_rtl_sda_o;
+  wire iic_rtl_sda_t;
+  wire myclk;
+  wire myreset;
+  wire uart0_rxd;
+  wire uart0_txd;
+
+  design_1 design_1_i
+       (.iic_rtl_scl_i(iic_rtl_scl_i),
+        .iic_rtl_scl_o(iic_rtl_scl_o),
+        .iic_rtl_scl_t(iic_rtl_scl_t),
+        .iic_rtl_sda_i(iic_rtl_sda_i),
+        .iic_rtl_sda_o(iic_rtl_sda_o),
+        .iic_rtl_sda_t(iic_rtl_sda_t),
+        .myclk(myclk),
+        .myreset(myreset),
+        .uart0_rxd(uart0_rxd),
+        .uart0_txd(uart0_txd));
+  IOBUF iic_rtl_scl_iobuf
+       (.I(iic_rtl_scl_o),
+        .IO(iic_rtl_scl_io),
+        .O(iic_rtl_scl_i),
+        .T(iic_rtl_scl_t));
+  IOBUF iic_rtl_sda_iobuf
+       (.I(iic_rtl_sda_o),
+        .IO(iic_rtl_sda_io),
+        .O(iic_rtl_sda_i),
+        .T(iic_rtl_sda_t));
+endmodule
